0c500c2062c877a60313e41fcfdf31491432c208
aleks24000 <alexandrmercier@gmail.com>
2019-06-06 12:52:43 +0200

    Initial commit

---
+++ Config.py
@@ -0,0 +1,15 @@
+#AUDIO_CONFIG = {
+#    'rate': 16000,
+#    'chunk': 2048,
+#    'record_device': 0,
+#    'play_device': 2,
+#    'channel': 2
+#}
+
+AUDIO_CONFIG = {
+    'rate': 44100,
+    'chunk': 2,
+    'record_device': 0,
+    'play_device': 1,
+    'channel': 2
+}
---
+++ drive_quickstart.py
@@ -0,0 +1,48 @@
+from __future__ import print_function
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from apiclient.http import MediaFileUpload
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/drive']
+
+def upload(audfile):
+    """Shows basic usage of the Drive v3 API.
+    Prints the names and ids of the first 10 files the user has access to.
+    """
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                os.getenv("HOME")+"/credentials.json", SCOPES)
+            creds = flow.run_local_server()
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('drive', 'v3', credentials=creds)
+
+    # Call the Drive v3 API
+    folder_id = '1-8AC7DqvkyRX5Ok7Io1XJRMhzfaEjZcA'
+    file_metadata = {'name': audfile+'.flac','parents': [folder_id]}
+    media = MediaFileUpload(audfile+'.flac',
+                            mimetype='audio/flac')
+    file = service.files().create(body=file_metadata,
+                                        media_body=media,
+                                        fields='id').execute()
+    print ("File ID: " + file.get('id'))
+
+if __name__ == '__main__':
+    upload("session-10")
